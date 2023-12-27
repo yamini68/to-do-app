27336018834e1efec22e89e4f053d5edb13e3faf
Murali <muralivvrsn75683@gmail.com>
2023-12-27 23:28:05 +0530

    added components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
-
+import "./App.css";
+import MyProjects from "./components/Tasks/Index";
+import TaskBoard from "./components/TaskBoard/Index";
 function App() {
   return (
-    <div className="flex absolute top-0 right-0 bottom-0 left-0">
-     
+    <div className="flex absolute top-0 right-0 bottom-0 left-0 overflow-hidden	">
+      <div className="w-60 bg-white shadow-custom">
+        <TaskBoard />
+      </div>
+      <div className="flex-grow">
+        <MyProjects />
+      </div>
     </div>
   );
 }
